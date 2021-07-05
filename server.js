e8d0bd426ebfea46f6be9b4b51c4a2bd209b1819
numan <numan.h.99@gmail.com>
2021-07-05 22:28:43 +0800

    resolve paths

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path'
 dotenv.config();
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import foodRoutes from './routes/foodRoutes.js';
@@ -24,9 +25,14 @@ app.use('/api/foods', foodRoutes);
 app.use('/api/restaurants', restaurantRoutes);
 app.use('/api/customers', customerRoutes);
 app.use('/api/orders', orderRoutes);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 app.use(notFound);
 app.use(errorHandler);
-
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
